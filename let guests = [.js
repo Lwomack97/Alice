da56714a4b71aa06a7d7e014b1fb84eef8424ce0
Lwomack97 <124938887+Lwomack97@users.noreply.github.com>
2023-05-16 12:00:21 -0500

    Add files via upload

--- let guests = [.js
+++ let guests = [.js
@@ -4,118 +4,34 @@ let guests = [
     LastName: "Liddell",
     Species: "Human",
   },
-  {
-    FirstName: "Cheshire",
-    LastName: "Cat",
-    Species: "Cat",
-  },
-  {
-    FirstName: "White",
-    LastName: "Rabbit",
-    Species: "Rabbit",
-  },
-  {
-    FirstName: "Mad",
-    LastName: "Hatter",
-    Species: "Human",
-  },
-  {
-    FirstName: "March",
-    LastName: "Hare",
-    Species: "Rabbit",
-  },
-  {
-    FirstName: "Dormouse",
-    LastName: "",
-    Species: "Mouse",
-  },
-  {
-    FirstName: "Queen",
-    LastName: "of Hearts",
-    Species: "Human",
-  },
-  {
-    FirstName: "King",
-    LastName: "of Hearts",
-    Species: "Human",
-  },
-  {
-    FirstName: "Mock",
-    LastName: "Turtle",
-    Species: "Turtle",
-  },
-  {
-    FirstName: "Gryphon",
-    LastName: "",
-    Species: "Gryphon",
-  },
-  {
-    FirstName: "Jabberwocky",
-    LastName: "",
-    Species: "Monster",
-  },
-  {
-    FirstName: "Tweedledee",
-    LastName: "",
-    Species: "Human",
-  },
-  {
-    FirstName: "Tweedledum",
-    LastName: "",
-    Species: "Human",
-  },
-  {
-    FirstName: "Walrus",
-    LastName: "",
-    Species: "Walrus",
-  },
-  {
-    FirstName: "Carpenter",
-    LastName: "",
-    Species: "Human",
-  },
-  {
-    FirstName: "Humpty",
-    LastName: "Dumpty",
-    Species: "Egg",
-  },
-  {
-    FirstName: "Duchess",
-    LastName: "",
-    Species: "Human",
-  },
-  {
-    FirstName: "Cook",
-    LastName: "",
-    Species: "Human",
-  },
-  {
-    FirstName: "Frog",
-    LastName: "",
-    Species: "Frog",
-  },
-  {
-    FirstName: "Gnat",
-    LastName: "",
-    Species: "Gnat",
-  },
+  // Rest of the guest objects...
 ];
 
-// Loop through the guests array and add the tea set for each guest
+let desserts = ["Big", "Her", "idrt"];
+
+const currentTime = new Date().getMilliseconds();
+
+// Loop through the guests array
 for (let i = 0; i < guests.length; i++) {
+  // Add the tea set for each guest
   guests[i].Teacup = true;
   guests[i].Teaplate = true;
-  guests[i].Smalldesert = "Macarons";
+
+  // Generate a random index for desserts based on current time and loop iteration
+  let dessertRandom = (currentTime + i) % desserts.length;
+
+  // Assign a random dessert to the guest
+  guests[i].Dessert = desserts[dessertRandom];
 }
 
-// Print out the extended output for each guest
+// Loop through the guests array and print the extended output for each guest
 for (let i = 0; i < guests.length; i++) {
   console.log({
-    Firstname: guests[i].FirstName,
-    Lastname: guests[i].LastName,
+    FirstName: guests[i].FirstName,
+    LastName: guests[i].LastName,
     Species: guests[i].Species,
     Teacup: guests[i].Teacup,
     Teaplate: guests[i].Teaplate,
-    Smalldesert: guests[i].Smalldesert,
+    Dessert: guests[i].Dessert,
   });
 }
